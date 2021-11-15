0334ed7e0c8405d4377a1cdbe6ffd4d515c7b8e9
Arif Mehniyar <arif.mehniyar@gmail.com>
2021-11-15 03:55:43 +0300

    feat. project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <smart-loader></smart-loader>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import SmartLoader from "@/components/SmartLoader"
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {SmartLoader},
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
